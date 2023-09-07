bd15c2ec840b6f6ec444ed57e613ccfc0d27a074
ciphrd <ciphrd@fxhash.xyz>
2023-09-07 04:56:39 +0200

    cleanup of Buffer + added chunk tests

---
+++ packages/onchfs-js/src/chunks.test.ts
@@ -0,0 +1,40 @@
+import { chunkBytes } from "./chunks"
+import { keccak } from "./utils"
+
+const u8 = (...arr: number[]) => new Uint8Array(arr)
+
+describe("chunking some bytes", () => {
+  test("empty buffer doesn't return any chunk", () => {
+    expect(chunkBytes(u8())).toHaveLength(0)
+  })
+
+  test("chunking 2 bytes with chunkskize=1 produces 2 chunks of 1 byte", () => {
+    expect(chunkBytes(u8(0, 1), 1)).toEqual([
+      {
+        bytes: u8(0),
+        hash: keccak(u8(0)),
+      },
+      {
+        bytes: u8(1),
+        hash: keccak(u8(1)),
+      },
+    ])
+  })
+
+  test("chunking into unequals chunks leaves smaller last chunk", () => {
+    expect(chunkBytes(u8(0, 1, 2), 2)).toEqual([
+      {
+        bytes: u8(0, 1),
+        hash: keccak(u8(0, 1)),
+      },
+      {
+        bytes: u8(2),
+        hash: keccak(u8(2)),
+      },
+    ])
+  })
+
+  test("chunk size of 0 should throw", () => {
+    expect(() => chunkBytes(u8(0), 0)).toThrow()
+  })
+})
--- packages/onchfs-js/src/utils.ts
+++ packages/onchfs-js/src/utils.ts
@@ -1,22 +1,6 @@
 import { keccak256 } from "js-sha3"
 
 /**
- * Instanciate a new buffer and copy the bytes from a source buffer in a
- * given range.
- * @param source The source to copy from
- * @param offset Offset in the source
- * @param length Number of bytes to copy after the offset. If empty, will copy
- * everything after the offset.
- * @returns A new buffer, with bytes copied from the source in given interval
- */
-export function BufferCopyFrom(source: Buffer, offset = 0, length?: number) {
-  length = typeof length === "undefined" ? source.byteLength - offset : length
-  const out = Buffer.alloc(length)
-  source.copy(out, 0, offset, offset + length)
-  return out
-}
-
-/**
  * Intanciates a new Uint8Array in which the requested bytes from the source
  * are copied into.
  * @param source The source to copy from
